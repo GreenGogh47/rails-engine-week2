3fef98bfb621b390d90b0ec279a72b1f6e1d1cb6
Chris Crane <chrisjcrane@gmail.com>
2023-03-27 14:56:53 -0600

    added sad path test for failed query as extension

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -1,26 +1,45 @@
 require "rails_helper"
 
 describe "Customers API" do
-  it "sends a list of customers" do
-    create_list(:customer, 3)
+  describe "happy path" do
+    it "sends a list of customers" do
+      create_list(:customer, 3)
 
-    get "/api/v1/customers"
+      get "/api/v1/customers"
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    customers = JSON.parse(response.body, symbolize_names: true)
+      customers = JSON.parse(response.body, symbolize_names: true)
 
-    expect(customers.count).to eq(3)
+      expect(customers.count).to eq(3)
 
-    customers.each do |customer|
-      expect(customer).to have_key(:id)
-      expect(customer[:id]).to be_an(Integer)
+      customers.each do |customer|
+        expect(customer).to have_key(:id)
+        expect(customer[:id]).to be_an(Integer)
 
-      expect(customer).to have_key(:first_name)
-      expect(customer[:first_name]).to be_a(String)
+        expect(customer).to have_key(:first_name)
+        expect(customer[:first_name]).to be_a(String)
 
-      expect(customer).to have_key(:last_name)
-      expect(customer[:last_name]).to be_a(String)
+        expect(customer).to have_key(:last_name)
+        expect(customer[:last_name]).to be_a(String)
+      end
+    end
+
+    xdescribe "sad path - extension" do
+      it "sends a list of customers" do
+        create_list(:customer, 3)
+  
+        get "/api/v1/customers"
+  
+        expect(response).to be({
+          "message": "your query could not be completed",
+          "errors": [
+            "string of error message one",
+            "string of error message two",
+            "etc"
+          ]
+        })
+      end
     end
   end
 end
