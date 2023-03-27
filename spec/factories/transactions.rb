1ce995c386d72ce8b6b0441494479bd5a0385576
Chris Crane <chrisjcrane@gmail.com>
2023-03-27 14:29:46 -0600

    set up factories

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :transaction do
-    
+    credit_card_number { Faker::Finance.credit_card(:visa) }
+    credit_card_expiration_date { "2024-6-9" } # nil in csv file?
+    result { Faker::Number.between(from: 0, to: 1) }
   end
 end
