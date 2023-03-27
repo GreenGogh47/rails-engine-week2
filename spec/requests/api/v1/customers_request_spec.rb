15726b57e0a31f25375580a9f1d7651c5994a83b
Chris Crane <chrisjcrane@gmail.com>
2023-03-27 14:50:08 -0600

    test total customers and assertions for response.body

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -7,5 +7,20 @@ describe "Customers API" do
     get "/api/v1/customers"
 
     expect(response).to be_successful
+
+    customers = JSON.parse(response.body, symbolize_names: true)
+
+    expect(customers.count).to eq(3)
+
+    customers.each do |customer|
+      expect(customer).to have_key(:id)
+      expect(customer[:id]).to be_an(Integer)
+
+      expect(customer).to have_key(:first_name)
+      expect(customer[:first_name]).to be_a(String)
+
+      expect(customer).to have_key(:last_name)
+      expect(customer[:last_name]).to be_a(String)
+    end
   end
 end
