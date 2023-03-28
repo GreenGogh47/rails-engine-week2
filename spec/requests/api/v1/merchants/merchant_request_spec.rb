6461d5823d975205b786cbaa9043e4f2970ee905
Chris Crane <chrisjcrane@gmail.com>
2023-03-28 13:30:37 -0600

    completed serializer and test for merchant_items

--- spec/requests/api/v1/merchants/merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_request_spec.rb
@@ -71,21 +71,21 @@ describe "Merchants API" do
           expect(response).to be_successful
     
           merchant_items = JSON.parse(response.body, symbolize_names: true)
+
+          expect(merchant_items[:data].count).to eq(4)
+
+          merchant_items[:data].each do |item|
+            expect(item).to have_key(:id)
+            expect(item[:id]).to be_a(String)
     
-          expect(merchant_items.count).to eq(4)
+            expect(item[:attributes]).to have_key(:name)
+            expect(item[:attributes][:name]).to be_a(String)
     
-          merchant_items.each do |merchant_item|
-            expect(merchant_item).to have_key(:id)
-            expect(merchant_item[:id]).to be_an(Integer)
+            expect(item[:attributes]).to have_key(:description)
+            expect(item[:attributes][:description]).to be_a(String)
     
-            expect(merchant_item).to have_key(:name)
-            expect(merchant_item[:name]).to be_a(String)
-    
-            expect(merchant_item).to have_key(:description)
-            expect(merchant_item[:description]).to be_a(String)
-    
-            expect(merchant_item).to have_key(:unit_price)
-            expect(merchant_item[:unit_price]).to be_a(Float)
+            expect(item[:attributes]).to have_key(:unit_price)
+            expect(item[:attributes][:unit_price]).to be_a(Float)
           end
         end
       end
