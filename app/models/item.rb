42efa25516b6727866fff727b552e20026707248
Chris Crane <chrisjcrane@gmail.com>
2023-03-29 13:19:04 -0600

    add model item model method find_by_name_fragment

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   validates_presence_of :name, :description, :unit_price, :merchant_id
+
+  def self.find_by_name_fragment(fragment)
+    where("name ILIKE ?", "%#{fragment}%").order(:name).first
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :unit_price }
     it { should validate_presence_of :merchant_id }
   end
+
+  describe 'class methods' do
+    describe '::find_by_name_fragment' do
+      it 'returns the first (alpha) item that matches the fragment' do
+        merchant = create(:merchant)
+        item_1 = create(:item, name: 'Timmys Adventure', merchant: merchant)
+        item_2 = create(:item, name: 'Johns Adventure', merchant: merchant)
+        item_3 = create(:item, name: 'Allisons Adventure', merchant: merchant)
+
+        expect(Item.find_by_name_fragment('adventure')).to eq(item_3)
+      end
+    end
+  end
 end
