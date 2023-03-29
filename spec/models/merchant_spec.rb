ccb1808fc2c637c131e2049143f5f2c29562acbe
Chris Crane <chrisjcrane@gmail.com>
2023-03-29 13:48:43 -0600

    add validation to merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :customers }
     it { should have_many :transactions }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
 end
